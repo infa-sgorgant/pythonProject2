a3bfe619e01b9e4ee9404664d9eadb4411fd0114
infa-sgorgant <udday.shank@gmail.com>
2023-08-25 01:36:34 +0530

    failed unit test

--- tests/helloTest.py
+++ tests/helloTest.py
@@ -11,7 +11,7 @@ class TestMain(unittest.TestCase):
         firstName = 'Alice'
         lastName = 'Wonder'
         response = helloUser(firstName,lastName)
-        expected = 'Hi Alice Wonder'
+        expected = 'Hi Alice Wonder1'
         self.assertEqual(response, expected)
 
 if __name__ == '__main__':
