d8dfdea67312b48cf55286467751be74438c0bef
infa-sgorgant <udday.shank@gmail.com>
2023-08-27 20:43:35 +0530

    failed unit test

--- src/hello.py
+++ src/hello.py
@@ -4,6 +4,6 @@ import main
 #input_filename = '../inputfile/sample.csv'
 
 def helloUser(firstName, lastname):
-    fullName = firstName + "  " + lastname
+    fullName = firstName + " 1 " + lastname
 
     return main.print_hi(fullName)
